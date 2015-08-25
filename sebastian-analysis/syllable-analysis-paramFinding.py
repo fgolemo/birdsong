9d1a1ece8a192e5226a3bb30ecc9d6d70ff7cc8a
fgolemo <fgolemo@gmail.com>
2015-08-25 18:28:53 +0200

    added new analysis tests and new sound files

--- sebastian-analysis/syllable-analysis-paramFinding.py
+++ sebastian-analysis/syllable-analysis-paramFinding.py
@@ -9,28 +9,30 @@ from scikits.audiolab import wavread, Format, Sndfile
 
 __author__ = 'Florian'
 
+zero_val = 1.23e-12  # the value that wave data gets set to if it's not a syllable
+
 # Params for bird 1136
 # amp_threshold = 1e-09  # if the amp of any freq is higher than this, it will be counted as good
 # freq_threshold = 8  # if the number of good freqs (see prev line) is higher than this, it's probably birdsong
 # smoothing = 12  # HAS TO BE >1, also keep the bins N to the left and right of good segments
 
 # Params for bird 1592
-# amp_threshold = 4.1e-08  # if the amp of any freq is higher than this, it will be counted as good
-# freq_threshold = 2  # if the number of good freqs (see prev line) is higher than this, it's probably birdsong
-# smoothing = 12  # HAS TO BE >1, also keep the bins N to the left and right of good segments
-# zero_val = 1.23e-12  # the value that wave data gets set to if it's not a syllable
+amp_threshold = 4.1e-08  # if the amp of any freq is higher than this, it will be counted as good
+freq_threshold = 2  # if the number of good freqs (see prev line) is higher than this, it's probably birdsong
+smoothing = 12  # HAS TO BE >1, also keep the bins N to the left and right of good segments
+
 
 # Params for bird 112
 # amp_threshold = 1e-08  # if the amp of any freq is higher than this, it will be counted as good
 # freq_threshold = 8  # if the number of good freqs (see prev line) is higher than this, it's probably birdsong
 # smoothing = 5  # HAS TO BE >1, also keep the bins N to the left and right of good segments
-# zero_val = 1.23e-12  # the value that wave data gets set to if it's not a syllable
+
 
 # Params for bird 1233
-amp_threshold = 1e-08  # if the amp of any freq is higher than this, it will be counted as good
-freq_threshold = 7  # if the number of good freqs (see prev line) is higher than this, it's probably birdsong
-smoothing = 4  # HAS TO BE >1, also keep the bins N to the left and right of good segments
-zero_val = 1.23e-12  # the value that wave data gets set to if it's not a syllable
+# amp_threshold = 1e-08  # if the amp of any freq is higher than this, it will be counted as good
+# freq_threshold = 7  # if the number of good freqs (see prev line) is higher than this, it's probably birdsong
+# smoothing = 4  # HAS TO BE >1, also keep the bins N to the left and right of good segments
+
 
 if smoothing <= 1:
     quit("smoothing has to be >1")
@@ -41,7 +43,8 @@ else:
     if len(sys.argv) > 2:
         quit("only one optional argument: path to wav file that is to be analyzed")
     else:
-        inFile = 'b1136.d94.t282148.wav'
+        # inFile = 'b1136.d94.t282148.wav'
+        inFile = 'b1592.d54.t66047281.wav'
 
 data, sample_freq, encoding = wavread(inFile)
 
