ce1129f7c232852f318b467aac34b9a357b4d43d
fgolemo <fgolemo@gmail.com>
2015-08-26 14:54:13 +0200

    completed flacCopier

--- sebastian-converter/flacCopier.py
+++ sebastian-converter/flacCopier.py
@@ -1,10 +1,10 @@
 from __future__ import print_function
-import fnmatch
+import shutil
 import os
 import sys
 
-filepath = "D:/birddata"
-outpath = "D:/birddata2"
+filepath = "/media/florian/My Passport/birddata-wav"
+outpath = "/media/florian/My Passport/birddata-flac"
 if len(sys.argv) == 3:
     filepath = os.path.abspath(os.path.expanduser(sys.argv[1]))
 
@@ -14,6 +14,7 @@ print("scanning directories for FLAC files")
 dirsToMake = []
 copyFiles = []
 
+print("creating index of files to copy")
 birds = os.listdir(filepath)
 for bird in birds:
     birdpath = filepath + os.path.sep + bird
@@ -30,11 +31,21 @@ for bird in birds:
         for song in songs:
             if song[-4:] == "flac":
                 copyFiles.append([bird, day, song])
+print("indexing done")
 
+print("making directories")
 for directory in dirsToMake:
     os.makedirs(outpath + directory, exist_ok=True)
+print("making directories done")
 
+i = 0
+print("transferring files")
 for copyFile in copyFiles:
+    src = filepath + os.sep + copyFile[0] + os.sep + copyFile[1] + os.sep + copyFile[2]
+    dest = outpath + os.sep + copyFile[0] + os.sep + copyFile[1] + os.sep + copyFile[2]
+    shutil.copy2(src, dest)
+    i += 1
+    percentDone = float(i) / len(copyFiles) * 100
+    print("{:.2f}% done".format(percentDone), end='\r')
 
-    pass
-
+print("\ntransferring files done")
