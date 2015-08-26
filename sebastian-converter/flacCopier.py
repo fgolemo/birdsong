a196ad3f2a349745a0d83078c4d36e224b7677fc
Florian Golemo <fgolemo@gmail.com>
2015-08-26 15:16:18 +0200

    improved version of the copier

--- sebastian-converter/flacCopier.py
+++ sebastian-converter/flacCopier.py
@@ -3,10 +3,11 @@ import shutil
 import os
 import sys
 
-filepath = "/media/florian/My Passport/birddata-wav"
-outpath = "/media/florian/My Passport/birddata-flac"
+filepath = "E:\\birddata-wav"
+outpath = "E:\\birddata-flac"
 if len(sys.argv) == 3:
     filepath = os.path.abspath(os.path.expanduser(sys.argv[1]))
+    outpath = os.path.abspath(os.path.expanduser(sys.argv[2]))
 
 matches = []
 print("scanning directories for FLAC files")
@@ -35,7 +36,8 @@ print("indexing done")
 
 print("making directories")
 for directory in dirsToMake:
-    os.makedirs(outpath + directory, exist_ok=True)
+    if not os.path.isdir(outpath + directory):
+        os.makedirs(outpath + directory)
 print("making directories done")
 
 i = 0
@@ -43,9 +45,15 @@ print("transferring files")
 for copyFile in copyFiles:
     src = filepath + os.sep + copyFile[0] + os.sep + copyFile[1] + os.sep + copyFile[2]
     dest = outpath + os.sep + copyFile[0] + os.sep + copyFile[1] + os.sep + copyFile[2]
-    shutil.copy2(src, dest)
     i += 1
     percentDone = float(i) / len(copyFiles) * 100
     print("{:.2f}% done".format(percentDone), end='\r')
+    if os.path.exists(dest) and os.path.getsize(dest) == os.path.getsize(src):
+        continue
+    else:
+        if os.path.exists(dest) and os.path.getsize(dest) != os.path.getsize(src):
+            os.remove(dest)
+    shutil.copy2(src, dest)
+
 
 print("\ntransferring files done")
