4d667b3d4e25bab8096949d282c64b448a422c44
Florian Golemo <fgolemo@gmail.com>
2015-11-08 00:34:08 +0100

    added tutor song storage to hearing component

--- architecture/components/hearing.py
+++ architecture/components/hearing.py
@@ -3,10 +3,18 @@ import os
 
 
 class Hearing:
-    def __init__(self):
-        pass
+    def __init__(self, tutorsong):
+        self.tutorsong = False
+        self.mfcc = False
+        if tutorsong != None and tutorsong != False:
+            self.tutorsong = tutorsong
 
     def calcMfcc(self, soundFile):
+        if self.tutorsong != False:
+            soundFile = self.tutorsong
+            if self.mfcc != False:
+                return self.mfcc
+
         if not os.path.isfile(soundFile):
             print "ERR: sound file couldn't be found:" + str(soundFile)
             return False
@@ -17,11 +25,12 @@ class Hearing:
         mfcc = librosa.feature.mfcc(y=data, sr=sr, n_mfcc=13)
 
         # discard the first row (amplitude) and evey row higher than 4, because they are mostly noise & artefacts
-        return mfcc[1:4, :]
+        self.mfcc = mfcc[1:4, :]
+        return self.mfcc
 
 
 if __name__ == "__main__":
-    h = Hearing()
+    h = Hearing(tutorsong=False)
 
     testFile = "../../sebastian-analysis/audio-samples/b1592.d91.t65217093.flac"
 
